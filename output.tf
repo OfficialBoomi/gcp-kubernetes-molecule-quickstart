a200ff0a5ace1142bbd38a2c4f56332045bb0815
Ganesh-Yeole <92855225+Ganesh-Yeole@users.noreply.github.com>
2022-09-27 11:33:24 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,3 @@
+output "NOTE"{   
+ value =  "Each deployment takes about 20-25 minutes to complete. It may take another 2 minutes for molecule to show up on the platform after the deployment is completed"
+ }
